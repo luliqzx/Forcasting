6bd7f1e1615fe59840eb8d2f132321fba59b57c5
luliqzx <luliqzx@gmail.com>
2017-12-17 19:27:02 +0700

    Add Order Entity

---
+++ Forcasting/Entity/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forcasting.Entity
+{
+    public class OrderDetail
+    {
+        public string OrderID { get; set; }
+        public int Line { get; set; }
+        public string Product { get; set; }
+        public string ProductName { get; set; }
+        public decimal Qty { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Forcasting/Entity/OrderHeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forcasting.Entity
+{
+    public class OrderHeader
+    {
+        public string OrderID { get; set; }
+        public string OrderDate { get; set; }
+        public string CustID { get; set; }
+        public string CustName { get; set; }
+        public decimal NetValue { get; set; }
+    }
+}
